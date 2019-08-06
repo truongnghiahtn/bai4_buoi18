c7e97d3f3a77d5cb231f002f459ecd2af77742d1
truongnghia <truongnghiahtn@gmail.com>
2019-08-06 15:27:59 +0700

    update

---
+++ src/app/baitap4/baitap4-product-item/baitap4-product-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-baitap4-product-item',
+  templateUrl: './baitap4-product-item.component.html',
+  styleUrls: ['./baitap4-product-item.component.scss']
+})
+export class Baitap4ProductItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
