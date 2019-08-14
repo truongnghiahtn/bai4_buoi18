d6bcb487cab48b0a9795804009a50f9e28e67f3c
truongnghia <truongnghiahtn@gmail.com>
2019-08-14 15:49:22 +0700

    update

--- src/app/baitap4/baitap4-smartphone/baitap4-smartphone.component.ts
+++ src/app/baitap4/baitap4-smartphone/baitap4-smartphone.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-baitap4-smartphone',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./baitap4-smartphone.component.scss']
 })
 export class Baitap4SmartphoneComponent implements OnInit {
-
+  @Output() eventshow_parent = new EventEmitter();
+  mangsmartphone :any[]=[
+    {TenSP:"IPhoneX",Chitiet:"iPhone X features a new all-screen design. Face ID, which makes your face your password",hinhAnh:"assets/img/sp_iphoneX.png",Gia:"800"},
+    {TenSP:"Galaxy Note7",Chitiet:"The Galaxy Note7 comes with a perfectly symmetrical design for good reason",hinhAnh:"assets/img/sp_note7.png",Gia:"900"},
+    {TenSP:"Vivo",Chitiet:"A young global smartphone brand focusing on introducing perfect sound quality",hinhAnh:"assets/img/sp_vivo850.png ",Gia:"700"},
+    {TenSP:"Blackberry",Chitiet:"BlackBerry is a line of smartphones, tablets, and services originally designed",hinhAnh:"assets/img/sp_blackberry.png",Gia:"1200"}
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+  detail(sanpham){
+    this.eventshow_parent.emit(sanpham);
   }
 
 }
--- src/app/baitap4/baitap4.module.ts
+++ src/app/baitap4/baitap4.module.ts
@@ -9,12 +9,13 @@ import { Baitap4FooterComponent } from './baitap4-footer/baitap4-footer.componen
 import { Baitap4FooterBrandComponent } from './baitap4-footer/baitap4-footer-brand/baitap4-footer-brand.component';
 import { Baitap4ProductItemComponent } from './baitap4-product-item/baitap4-product-item.component';
 import { Baitap4LaptopComponent } from './baitap4-laptop/baitap4-laptop.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 
 
 @NgModule({
-  declarations: [Baitap4Component, Baitap4HeaderComponent, Baitap4CarouselComponent, Baitap4SmartphoneComponent,  Baitap4FooterComponent, Baitap4FooterBrandComponent, Baitap4ProductItemComponent, Baitap4LaptopComponent],
+  declarations: [Baitap4Component, Baitap4HeaderComponent, Baitap4CarouselComponent, Baitap4SmartphoneComponent,  Baitap4FooterComponent, Baitap4FooterBrandComponent, Baitap4ProductItemComponent, Baitap4LaptopComponent, ModalComponent],
   exports :[Baitap4Component],
   imports: [
     CommonModule
